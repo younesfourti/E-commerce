988051935bf0da6bb851300108888d2e6cfb03ba
younes <>
2020-05-06 09:22:54 +0200

    voila ce que j ai fais je galere

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+        <meta charset="utf-8">
+      <title>LensShop</title>
+</head>
+
+
+<body>
+
+<?php include_once '\app\Views\navtohome.html';?>
+
+<div>
+Nos objectifs
+</div>
+<h1>Si vous savez ce que vous cherchez, vous pouvez parcourir notre gamme d'objectifs EF, RF, EF-S et EF-M pour trouver votre objectif idéal.</h1>
+
+
+
+
+</body>
+
+
+
+</html>
