5c0a9c3a18bbf3201dc63e8a2a6ef54c3c91ea08
jameszuballers <jameszuballers@gmail.com>
2020-05-06 10:09:38 +0200

    style 06 05 2020

--- index.php
+++ index.php
@@ -7,16 +7,27 @@
 
 
 <body>
+  <style>
+    .image{
 
-<?php include_once '\app\Views\navtohome.html';?>
+      background:url("public/assets/img/l_series_promo_product_1280.jpg") no-repeat center fixed;
+      background-size: cover;
+      width: 100%;
+      height: 40rem;
+      margin: 0;
+      padding: 0;
+    }
 
-<div>
-Nos objectifs
+  </style>
+<div class="image">
+
+  <?php include_once '\app\Views\navtohome.html';?>
+
+  <div>
+    <p>Nos objectifs</p>
+    <h1 style="position:relative; top:50px;font-size: 40px; text-align:center;color:white;">Si vous savez ce que vous cherchez, vous pouvez parcourir notre gamme d'objectifs EF, RF, EF-S et EF-M pour trouver votre objectif idéal.</h1>
+  </div>
 </div>
-<h1>Si vous savez ce que vous cherchez, vous pouvez parcourir notre gamme d'objectifs EF, RF, EF-S et EF-M pour trouver votre objectif idéal.</h1>
-
-
-
 
 </body>
 
