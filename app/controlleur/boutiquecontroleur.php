68d527f63fd862055908cd0455ef8f2a32f70976
younes <>
2020-05-21 16:33:13 +0200

    younes bug réparation

---
+++ app/controlleur/boutiquecontroleur.php
@@ -0,0 +1,32 @@
+
+
+
+
+<?php
+session_start();
+// require_once('defaultcontroleur.php');
+class boutique extends Controller {
+
+    public function boutique(){
+        
+         
+        $this->render('boutique');
+        
+    }
+    public function article(){
+        
+         
+        $this->render('article');
+        
+    }
+    
+        
+
+        
+        
+
+        
+
+    }
+
+
