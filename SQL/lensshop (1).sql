6f4a98dba4cdc7ddc1954ba5c622d0575e7ac083
younes <>
2020-05-24 15:50:20 +0200

    sql

--- SQL/lensshop (1).sql
+++ SQL/lensshop (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 21 Mai 2020 à 16:46
+-- Généré le :  Dim 24 Mai 2020 à 15:46
 -- Version du serveur :  10.1.21-MariaDB
 -- Version de PHP :  5.6.30
 
@@ -19,6 +19,34 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `lensshop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `idarticle` int(11) NOT NULL,
+  `titre` text NOT NULL,
+  `descri` text NOT NULL,
+  `prix` int(11) NOT NULL,
+  `photo` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `article`
+--
+
+INSERT INTO `article` (`idarticle`, `titre`, `descri`, `prix`, `photo`) VALUES
+(1, 'Adaptateurs EOS R', 'Utilisez vos objectifs EF et EF-S existants avec le nouvel appareil photo EOS R et repoussez les limites de votre créativité.', 60, '../public\\assets\\img\\adaptors_318365189665423.jpg'),
+(2, 'EF-S', 'Des objectifs légers, offrant de superbes prises de vue aux appareils photo reflex avec capteurs APS-C.', 12, '../public\\assets\\img\\gamme ef-s.jpg'),
+(3, 'EF-M', 'Des objectifs légers et portables conçus exclusivement pour les appareils photo EOS M.', 12, '../public\\assets\\img\\ef-m.jpg'),
+(4, 'Focale fixe EF', 'Des objectifs associant qualité d\'image, taille compacte et grandes ouvertures maximales.', 12, '../public\\assets\\img\\gamme focal fix ef.jpg'),
+(5, 'Objectifs RF', 'Des objectifs permettant de changer de cadrage en tournant simplement la bague de zoom.', 12, '../public\\assets\\img\\Zoom_317348669854370.jpg'),
+(6, 'Macro', 'Conçu pour révéler les moindres détails - Parfait pour photographier les fleurs et les insectes.', 60, '../public\\assets\\img\\Macro_168733072721902.jpg'),
+(7, 'Bascule et décentrement', 'Contrôlez votre perspective et votre plan focal grâce à ces  objectifs spécialisés.', 12, '../public\\assets\\img\\Tilt-Shift_172194903893890.jpg'),
+(8, 'Multiplicateurs de focale', 'Augmentez la portée de votre objectif et rapprochez-vous de votre sujet.', 12, '../public\\assets\\img\\Extenders_158628571845866.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,6 +87,13 @@ INSERT INTO `compte` (`idCompte`, `email`, `password`, `firstname`, `lastname`, 
 --
 
 --
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`idarticle`),
+  ADD UNIQUE KEY `idarticle` (`idarticle`);
+
+--
 -- Index pour la table `compte`
 --
 ALTER TABLE `compte`
@@ -69,6 +104,11 @@ ALTER TABLE `compte`
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `idarticle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT pour la table `compte`
 --
